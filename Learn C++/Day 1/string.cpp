a5a81fdcbc7c0ae14db3c17c0cefd3761140d54d
Mahesh Patil <maheshpatilmp937@gmail.com>
2026-06-11 18:04:11 +0530

    first commit

---
+++ Learn C++/Day 1/string.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+#include  <string>
+int main() {
+   string favouritetea= "Herbal, neem tea \n" ;
+   string Description = " \"Lemon\"was very tasty"; // use / for write a word in "" 
+   cout << favouritetea << Description << endl;
+    return 0;
+}   
---
+++ Learn C++/Day 1/userInput.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+ string Teaname;
+ int Teaquantity;
+ cout << "Enter the Tea name: ";
+ getline (cin, Teaname);
+ cout << "Enter the Tea quantity: ";
+ cin >> Teaquantity;
+ cout << "Tea name: " << Teaname << endl;
+ cout << "Tea quantity: " << Teaquantity << endl;
+
+
+
+    return 0;
+}   
