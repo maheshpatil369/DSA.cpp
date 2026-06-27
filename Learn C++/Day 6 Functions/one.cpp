bf80f68b35de863be040c57e563495af445dadb9
Mahesh Patil <maheshpatilmp937@gmail.com>
2026-06-27 17:56:23 +0530

    changes

---
+++ Learn C++/Day 6 Functions/one.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    char ch = 1234343456;
+    cout << ch << endl;
+    return 0;
+}
+
+
+
+
+// Output 
+// [Running] cd "/home/maheshpatil369/Desktop/DSA C++/Learn C++/Day .. Functions/" && g++ myFunction.cpp -o myFunction && "/home/maheshpatil369/Desktop/DSA C++/Learn C++/Day .. Functions/"myFunction
+// Serving Masala Tea
